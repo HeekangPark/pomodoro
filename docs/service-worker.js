7e653990fe10b10e2050e9abb1581537939c483f
Heekang Park <park.heekang33@gmail.com>
2021-08-24 03:16:35 +0900

    v1.0.5

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,3 +1,6 @@
 importScripts("/pomodoro/precache-manifest.1984babf0e2dc473cdbd76d4c08fd762.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-console.log("test from service-worker.js")
+self.addEventListener('fetch', event => {
+    console.log("Fetching somthing!!", event.request.url);
+    event.respondWith(fetch(event.request));
+});
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,4 @@
-console.log("test from service-worker.js")
+self.addEventListener('fetch', event => {
+    console.log("Fetching somthing!!", event.request.url);
+    event.respondWith(fetch(event.request));
+});
